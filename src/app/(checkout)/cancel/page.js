ccfcd97b9648a6459f4272d03323d875e349588f
Jason-AML <jaml08@hotmail.com>
2026-05-29 11:22:45 -0500

    feat: Pagina de cancelacion checkout

---
+++ src/app/(checkout)/cancel/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from "next/link";
+const page = () => {
+  return (
+     <div className="flex flex-col items-center justify-center h-screen">
+      <span className="material-symbols-outlined collapse-open  text-green-500 mb-4 ">
+check_circle
+</span>
+      <h1>Checkout Cancelled</h1>
+      <Link href="/collection" className="btn btn-primary mt-6">
+        Back to Collection
+      </Link>
+    </div>
+  )
+}
+
+export default page
