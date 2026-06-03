212995e4904da6b17a495e8fcc6980650f76a1cb
Jason-AML <jaml08@hotmail.com>
2026-06-03 01:52:41 -0500

    delete: log de la sesion

--- src/app/collection/[carId]/Detail.jsx
+++ src/app/collection/[carId]/Detail.jsx
@@ -26,7 +26,6 @@ const Detail = ({ carId }) => {
       });
       const session = await response.json();
       window.location.href = session.url; 
-      console.log("Stripe session created:", session)
   };
   return (
     <section className="bg-[#0A0A0A] text-slate-800  min-h-screen pt-10">
