b470581f9eea6dcc2ff192249ff18762b3bba2f0
Jason-AML <jaml08@hotmail.com>
2026-06-07 08:39:29 -0500

    styles

--- src/app/(checkout)/error/page.js
+++ src/app/(checkout)/error/page.js
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import Link from "next/link";
 const page = () => {
   return (
-    <div>
-      <h1>Something went wrong</h1>
-      <p>We apologize for the inconvenience. Please try again later.</p>
+       <div className="flex flex-col items-center justify-center h-screen">
+      <span className="material-symbols-outlined">
+error
+</span>
+      <h1>Error</h1>
+      <p>No se ha podido continuar con la operación</p>
+      <Link href="/collection" className="btn btn-primary mt-6">
+        Back to Collection
+      </Link>
     </div>
   )
 }
