69b7536f090968558b0dbe93a97f299a4261b75a
Jason-AML <jaml08@hotmail.com>
2026-07-04 17:43:16 -0500

    limitacion en la carga de mensajes, se muestran unicamente los ultimos 8

--- src/hooks/useChat.js
+++ src/hooks/useChat.js
@@ -20,7 +20,7 @@ export function useChat() {
       .from('messages')
       .select('*')
       .eq('user_id', user.id)
-      .order('created_at', { ascending: true })
+      .order('created_at', { ascending: false }).limit(8)
       .then(({ data }) => setMessages(data || []))
   }, [user])
 
